afb51d5a32f341874a51ddeb4a7770d7433328c0
Bastian.hundt <bastian@hundt.me>
2022-02-17 17:29:56 +0100

    folder and deployment

--- deployment/deploy-local.sh
+++ deployment/deploy-local.sh
@@ -2,11 +2,12 @@
 
 source config/dev_env.sh
 
-# Copy to working folder
-cp feature_store.yaml $DIY_ML_P_REL_DEPLOY_PATH/$DIY_ML_P_ENVIRONMENT/feature-store/feature_store.yaml
+# clear working folder
+rm -r $DIY_ML_P_REL_DEPLOY_PATH/$DIY_ML_P_ENVIRONMENT/feature-store/*
 
-cp example.py $DIY_ML_P_REL_DEPLOY_PATH/$DIY_ML_P_ENVIRONMENT/feature-store/example.py
+# copy to working folder
+cp -a src/ $DIY_ML_P_REL_DEPLOY_PATH/$DIY_ML_P_ENVIRONMENT/feature-store/
 
-# execute feast apply
+# apply changes
 cd $DIY_ML_P_REL_DEPLOY_PATH/$DIY_ML_P_ENVIRONMENT/feature-store/
 feast apply
